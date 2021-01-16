2104fd1b8f49d10a0d3b7a7467eb71457d9baf20
junui <wnsspdl@naver.com>
2021-01-16 23:36:01 +0900

    ch12 자물쇠와열쇠3

--- src/main/java/simulation/자물쇠와열쇠.java
+++ src/main/java/simulation/자물쇠와열쇠.java
@@ -32,8 +32,9 @@ public class 자물쇠와열쇠 {
 
     public static boolean check(int[][] lock){
         boolean ret = true;
-        for (int i = 0; i < lock.length; ++i) {
-            for (int j = 0; j < lock.length; ++j) {
+        int lockLength = lock.length / 3;
+        for (int i = lockLength; i < lockLength * 2; ++i) {
+            for (int j = lockLength; j < lockLength * 2; ++j) {
                 if (lock[i][j] == 0) {
                     return false;
                 }
@@ -43,41 +44,38 @@ public class 자물쇠와열쇠 {
     }
 
 
-    public boolean solution(int[][] key, int[][] lock) {
+    public static boolean solution(int[][] key, int[][] lock) {
         boolean answer = false;
+        int n = lock.length;
+        int m = key.length;
         int[][] copyKey = key.clone();
+        int[][] newLock = new int[3 * n][3 * n];
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                newLock[i + n][j + n] = lock[i][j];
+            }
+        }
+
         for(int rotate = 0; rotate < 4; ++rotate) {
             copyKey = rotate(copyKey);
-            for (int i = 0; i < lock.length; ++i) {
-                for (int j = 0; j < lock.length; ++j) {
-                    int[][] copyLock = Arrays.stream(lock).map(int[]::clone).toArray(int[][]::new);
-                    for (int startI = i, keyI = 0; startI < lock.length && keyI < key.length; ++startI, ++keyI) {
-                        for (int startJ = j, keyJ = 0; startJ < lock.length && keyJ < key.length; ++startJ, ++keyJ) {
-                            if(copyKey[keyI][keyJ] == 1 && copyLock[startI][startJ] == 1){
-                                copyLock[startI][startJ] = 0;
-                                break;
+            for (int x = 0; x < n * 2; ++x) {
+                for (int y = 0; y < n * 2; ++y) {
+                    // 2차원 배열을 deepcopy 하는 방법 Java8
+                    int[][] copyLock = Arrays.stream(newLock).map(int[]::clone).toArray(int[][]::new);
+                    for(int i = 0; i < m; ++i){
+                        for(int j = 0; j < m; ++j){
+                            if(copyKey[i][j] == 1 && copyLock[x + i][y + j] == 0){
+                                copyLock[x + i][y + j] = 1;
+                                continue;
                             }
-                            if(copyKey[keyI][keyJ] == 1 && copyLock[startI][startJ] == 0){
-                                copyLock[startI][startJ] = 1;
+                            if(copyKey[i][j] == 1 && copyLock[x + i][y + j] == 1){
+                                copyLock[x + i][y + j] = 0;
                             }
                         }
                     }
                     //check
                     if(check(copyLock)){
-                        System.out.println(i + " " + j);
-                        for(int k = 0; k < copyLock.length; ++k){
-                            for(int l = 0; l < copyLock.length; ++l){
-                                System.out.print(copyKey[k][l]);
-                            }
-                            System.out.println();
-                        }
-                        System.out.println("");
-                        for(int k = 0; k < copyLock.length; ++k){
-                            for(int l = 0; l < copyLock.length; ++l){
-                                System.out.print(copyLock[k][l]);
-                            }
-                            System.out.println();
-                        }
                         return true;
                     }
                 }
