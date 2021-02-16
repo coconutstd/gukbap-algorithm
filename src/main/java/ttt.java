0b4c953be2c6457d65c6ab4ff0dec22eb06bccc8
junui <wnsspdl@naver.com>
2021-02-16 13:48:41 +0900

    바이러스

---
+++ src/main/java/ttt.java
@@ -0,0 +1,7 @@
+public class ttt {
+    public static void main(String[] args) {
+        String test = "abcd";
+        test = test.substring(1, test.length());
+        System.out.println(test);
+    }
+}
