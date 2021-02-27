b94b86b284b034cf6b204492a60216c82538a4ad
junui <wnsspdl@naver.com>
2021-02-27 13:33:26 +0900

    base code

---
+++ src/main/java/soma/A.java
@@ -0,0 +1,20 @@
+package soma;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class A {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
