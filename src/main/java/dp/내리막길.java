46502a63046249b0bf93291f48dd7ece8e7931af
junui <wnsspdl@naver.com>
2021-03-15 09:26:02 +0900

    내리막길

--- src/main/java/dp/내리막길.java
+++ src/main/java/dp/내리막길.java
@@ -1,11 +1,12 @@
+// 메모리초과 ..
 package dp;
 
 import java.util.*;
 import java.io.*;
 
-class Visit{
-    int x,y;
-    public Visit(int x, int y) {
+class Node{
+    int x, y;
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -13,36 +14,16 @@ class Visit{
 
 public class 내리막길 {
 
-    static int n, m;
-    static int[][] map;
-    static HashMap<Visit, Boolean> hashMap = new HashMap<>();
-    static int answer = 0;
-
-    public static void dp(int x, int y){
-        System.out.println(x + " " + y);
-        if(x < 0 || x >= n || y <  0 || y >= m) return;
-        if(x == n - 1 && y == m - 1){
-            answer++;
-        }
-        int cur = map[x][y];
-        if(x + 1 < n && cur > map[x + 1][y]){
-            dp(x + 1, y);
-        }
-        if(y + 1 < m && cur > map[x][y + 1]){
-            dp(x, y+ 1);
-        }
-        if(0 <= y - 1 && cur > map[x][y - 1]){
-            dp(x + 1, y);
-        }
-    }
+    static int[] dx = {0, 0, 1};
+    static int[] dy = {1, -1, 0};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
 
         for(int i = 0; i < n; ++i){
             st = new StringTokenizer(br.readLine());
@@ -50,14 +31,98 @@ public class 내리막길 {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        dp(0, 0);
+        int answer = 0;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0, 0));
+        while(!q.isEmpty()){
+            int x = q.peek().x;
+            int y = q.peek().y;
+            q.poll();
+            if(x == n - 1 && y == m - 1){
+                answer++;
+                continue;
+            }
+            for(int i = 0; i < 3; ++i){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(0 <= nx && nx < n && 0 <= ny && ny < m){
+                    if(map[x][y] > map[nx][ny])
+                        q.offer(new Node(nx, ny));
+                }
+            }
+        }
         bw.write(answer + "\n");
         bw.flush();
         bw.close();
         br.close();
     }
 }
+
+
+
+//package dp;
+//
+//import java.util.*;
+//import java.io.*;
+//
+//class Visit{
+//    int x,y;
+//    public Visit(int x, int y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//}
+//
+//public class 내리막길 {
+//
+//    static int n, m;
+//    static int[][] map;
+//    static HashMap<Visit, Boolean> hashMap = new HashMap<>();
+//    static int answer = 0;
+//
+//    public static void dp(int x, int y){
+//        System.out.println(x + " " + y);
+//        if(x < 0 || x >= n || y <  0 || y >= m) return;
+//        if(x == n - 1 && y == m - 1){
+//            answer++;
+//        }
+//        int cur = map[x][y];
+//        if(x + 1 < n && cur > map[x + 1][y]){
+//            dp(x + 1, y);
+//        }
+//        if(y + 1 < m && cur > map[x][y + 1]){
+//            dp(x, y+ 1);
+//        }
+//        if(0 <= y - 1 && cur > map[x][y - 1]){
+//            dp(x + 1, y);
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        n = Integer.parseInt(st.nextToken());
+//        m = Integer.parseInt(st.nextToken());
+//        map = new int[n][m];
+//
+//        for(int i = 0; i < n; ++i){
+//            st = new StringTokenizer(br.readLine());
+//            for(int j = 0; j < m; ++j){
+//                map[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        dp(0, 0);
+//        bw.write(answer + "\n");
+//        bw.flush();
+//        bw.close();
+//        br.close();
+//    }
+//}
+
+
+
 
 
 /*
