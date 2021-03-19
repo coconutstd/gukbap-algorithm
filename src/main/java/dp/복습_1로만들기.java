4ab86f9c59c20abcf50e39055751e1fe18422f86
junui <wnsspdl@naver.com>
2021-03-19 20:19:23 +0900

    1로만들기-메모-복습

--- src/main/java/dp/복습_1로만들기.java
+++ src/main/java/dp/복습_1로만들기.java
@@ -5,10 +5,13 @@ import java.io.*;
 
 public class 복습_1로만들기 {
 
+    static int[] cache;
+
     static int recursion(int x){
         if(x == 1){
             return 0;
         }
+        if(cache[x] != 0) return cache[x];
         int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, c = Integer.MAX_VALUE, d = Integer.MAX_VALUE;
         if(x % 5 == 0){
             a = recursion(x / 5);
@@ -16,17 +19,18 @@ public class 복습_1로만들기 {
         if(x % 3 == 0){
             b = recursion(x / 3);
         }
-        if(x % 3 == 0){
+        if(x % 2 == 0){
             c = recursion(x / 2);
         }
         d = recursion(x - 1);
-        return 1 + Math.min(Math.min(a, b), Math.min(c, d));
+        return cache[x] = 1 + Math.min(Math.min(a, b), Math.min(c, d));
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int x = Integer.parseInt(br.readLine());
+        cache = new int[x + 1];
         bw.write(recursion(x) + "\n");
         bw.flush();
         bw.close();
