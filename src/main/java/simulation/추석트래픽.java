a0d21f863ae09977cafe7c1767ef55402ad939de
junui <wnsspdl@naver.com>
2021-03-26 09:22:11 +0900

    다익스트라 복습-전보

--- src/main/java/simulation/추석트래픽.java
+++ src/main/java/simulation/추석트래픽.java
@@ -1,3 +1,4 @@
+
 package simulation;
 
 import soma.B;
@@ -81,7 +82,7 @@ public class 추석트래픽 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] input = new String[]{"2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"};
+        String[] input = new String[]{"2016-09-15 00:00:01.000 1s", "2016-09-15 00:00:02.000 1s", "2016-09-15 00:00:03.000 0.1s"};
         bw.write(solution(input) + "\n");
         bw.flush();
         bw.close();
