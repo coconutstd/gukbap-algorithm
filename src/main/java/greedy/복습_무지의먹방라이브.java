bcb22548b56a18aeb882c5b0711c399e357115ff
junui <wnsspdl@naver.com>
2021-04-01 23:17:17 +0900

    복습_무지의먹방라이브

---
+++ src/main/java/greedy/복습_무지의먹방라이브.java
@@ -0,0 +1,22 @@
+package greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class 복습_무지의먹방라이브 {
+
+    public static int solution(int[] food_times, long k) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
