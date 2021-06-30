f896a42a18d76f7e3d17bb4f58a44672c432d37e
junui <wnsspdl@naver.com>
2021-06-30 23:30:35 +0900

    메모리문제2

--- src/main/java/부스트캠프/memory.py
+++ src/main/java/부스트캠프/memory.py
@@ -5,13 +5,19 @@ input4 = ["BOOL", "LONG", "SHORT", "LONG", "BOOL", "LONG", "BOOL", "LONG", "SHOR
 
 def solution(input):
     ret = []
+    prev = ""
     for data in input:
         mem = []
-        if data == "INT":
+        if data == "FLOAT":
             for i in range(4):
                 mem.append("#")
-        ret.append(''.join(mem))
-
+            ret.append(''.join(mem))
+        elif data == "INT":
+            ret.append("########")
+        elif data == "LONG":
+            ret.append("########")
+            ret.append("########")
+        prev = data
     print(ret)
 
 solution(input)
