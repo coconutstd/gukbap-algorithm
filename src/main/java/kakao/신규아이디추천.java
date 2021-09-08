75aacaca2f4f9101ae1099dfb7d5bd1e17f6f7d2
junui <wnsspdl@naver.com>
2021-09-08 18:18:55 +0900

    신규아이디추천 2단계까지 해결

---
+++ src/main/java/kakao/신규아이디추천.java
@@ -0,0 +1,25 @@
+package kakao;
+
+import java.util.*;
+
+public class 신규아이디추천 {
+    public static void main(String[] args) {
+        String input_param = "...!@BaT#*..y.abcdefghijklm";
+        solution(input_param);
+    }
+    public static String solution(String new_id) {
+        String answer = "";
+        String processing_id = new_id;
+        processing_id.toLowerCase();
+
+        String temp_id = "";
+        for(int i = 0; i < processing_id.length(); ++i){
+            Character ch = processing_id.charAt(i);
+            if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9' || ch == '-' || ch == '_' || ch == '.') {
+                temp_id += ch;
+            }
+        }
+        System.out.println(temp_id);
+        return answer;
+    }
+}
