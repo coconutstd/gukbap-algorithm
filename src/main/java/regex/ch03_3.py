a49d5b950cd6384cd3800e02ca82dc68151c3522
junui <wnsspdl@naver.com>
2021-09-20 17:44:49 +0900

    정규표현식 문자집합 사용하기3 ^ 메타문자는 제외할 때

--- src/main/java/regex/ch03_3.py
+++ src/main/java/regex/ch03_3.py
@@ -6,12 +6,14 @@ example = "sales1.xls" \
           "sales3.xls" \
           "apac1.xls" \
           "europe2.xls" \
+          "sam.xls" \
           "na1.xls" \
           "na2.xls" \
           "sa1.xls" \
           "ca1.xls"
 
-regex = "[ns]a.\.xls"
+# regex = "[ns]a.\.xls"
+regex = "[ns]a[^0-9]\.xls"
 
 obj = re.compile(regex)
 print(obj.findall(example))
