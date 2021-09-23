8cdb588f98ab51e67faa2b0108f7de8fceeecb2e
junui <wnsspdl@naver.com>
2021-09-23 14:30:16 +0900

    정규표현식 * + {,} 메타문자에 ?를 붙이면 게으른 수량자로 변한다

--- src/main/java/regex/ch05_1.py
+++ src/main/java/regex/ch05_1.py
@@ -8,20 +8,30 @@ import re
 # obj = re.compile(regex)
 # print(obj.findall(example))
 
-example2 = """
-            body {
-                background-color: #fefbd8;
-            }
-            h1 {
-                background-color: #0000ff;
-            }
-            div {
-                background-color: #d0f4e6;
-            }
-            span {
-                background-color: #f08970;
-            }
-           """
-regex2 = "#[a-z0-9A-Z]{6}"
-obj = re.compile(regex2)
-print(obj.findall(example2))
+# example2 = """
+#             body {
+#                 background-color: #fefbd8;
+#             }
+#             h1 {
+#                 background-color: #0000ff;
+#             }
+#             div {
+#                 background-color: #d0f4e6;
+#             }
+#             span {
+#                 background-color: #f08970;
+#             }
+#            """
+# regex2 = "#[a-z0-9A-Z]{6}"
+# obj = re.compile(regex2)
+# print(obj.findall(example2))
+
+example3 = """
+            This offer is not available to customers
+            living in <b>AK</b> and <b>HI</b>.
+            """
+
+regex3 = "<[Bb].*?<\/[Bb]>"
+
+obj = re.compile(regex3)
+print(obj.findall(example3))
