21944cf90699d7805e3505733302d25f40a045fe
junui <wnsspdl@naver.com>
2021-09-27 23:50:21 +0900

    정규표현식 파이썬은 하위표현식에 대한 역참조가 안된다.

--- src/main/java/regex/ch08.py
+++ src/main/java/regex/ch08.py
@@ -1,12 +1,23 @@
 import re
 
-example = """
-This is a block of of text,
-several words here are are
-repeated, and and they
-should not be.
+# example = """
+# This is a block of of text,
+# several words here are are
+# repeated, and and they
+# should not be.
+# """
+#
+# regex = "[ ]+(\w+)[ ]+\1"
+# obj = re.compile(regex)
+# print(obj.findall(example))
+
+example2 = """
+    313-555-1234
+    248-555-9999
+    810-555-9000
 """
 
-regex = "[ ]+(\w+)[ ]+\1"
-obj = re.compile(regex)
-print(obj.findall(example))
+regex2 = "(\d{3})(-)(\d{3})(-)(\d{4)"
+obj2 = re.compile(regex2)
+print(re.sub(regex2, example2, "($1) $3-$5"))
+
