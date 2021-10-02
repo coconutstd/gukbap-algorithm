143b21bf94960ede9583da9f508ab518fb6adcd9
junui <wnsspdl@naver.com>
2021-10-02 15:41:00 +0900

    자료구조 스택 올바른코드

--- src/main/python/datastructure/Stack.py
+++ src/main/python/datastructure/Stack.py
@@ -7,3 +7,19 @@ class Node:
 class Stack:
     def __init__(self):
         self.last = None
+
+    def push(self, item):
+        self.last = Node(item, self.last)
+
+    def pop(self):
+        item = self.last.item
+        self.last = self.last.next
+        return item
+
+
+st = Stack()
+st.push(1)
+st.push(2)
+
+print(st.pop())
+print(st.pop())
