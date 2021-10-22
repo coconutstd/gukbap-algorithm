1a6995b56864a60d9ba70f970503d91c12ad093f
junui <wnsspdl@naver.com>
2021-10-22 16:42:53 +0900

    자료구조 트리 반복적으로 순회하기 TODO: 후위순회 구현

--- src/main/java/tree/Tree.java
+++ src/main/java/tree/Tree.java
@@ -13,16 +13,16 @@ class Node {
 
 public class Tree {
     public static void main(String[] args) {
-        Node root = new Node(3);
-        root.left = new Node(1);
-        root.right = new Node(5);
-        root.left.left = new Node(6);
-        root.left.right = new Node(7);
-        root.right.left = new Node(2);
-        root.right.right = new Node(4);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
         Tree tree = new Tree();
-        tree.ldr1(root);
+        tree.ldr3(root);
     }
 
     private void ldr1(Node root) {
@@ -35,9 +35,28 @@ public class Tree {
 
     private void ldr2(Node root) {
         Stack<Node> stack = new Stack<>();
-        stack.push(root);
         while (true) {
+           while(root != null) {
+               stack.push(root);
+               System.out.println(root.value);
+               root = root.left;
+           }
+           if(stack.isEmpty()) break;
+           root = stack.pop();
+           root = root.right;
+        }
+    }
 
+    private void ldr3(Node root) {
+        Stack<Node> stack = new Stack<>();
+        while (true) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            if(stack.isEmpty()) break;
+            root = stack.pop();
+            root = root.right;
         }
     }
 }
