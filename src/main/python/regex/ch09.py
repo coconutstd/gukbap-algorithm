2dbc4abe595ee389d12751091e0300ed12f4ddc9
junui <wnsspdl@naver.com>
2021-11-02 18:18:01 +0900

    정규표현식 전방탐색은 ?= 후방탐색은 ?<=

--- src/main/python/regex/ch09.py
+++ src/main/python/regex/ch09.py
@@ -1,10 +1,17 @@
 import re
 
 example = """
-    http://www.forta.com/
-    https://mail.forta.com/
-    ftp://ftp.forta.com/
+http://www.forta.com/
+https://mail.forta.com/
+ftp://ftp.forta.com/
 """
 
-regex = "\w+(?=:)"
-print(re.findall(regex, example))
+print(re.findall(r".+(?=:)", example))
+
+example2 = "ABC01: $23.45" \
+           "HGG42: $5.31" \
+           "CFMX1: $899.00" \
+           "XTC99: $69.96" \
+           "Total items found: 4"
+
+print(re.findall(r"(?<=\$)[0-9.]+", example2))
